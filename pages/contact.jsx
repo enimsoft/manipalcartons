d72009d107566a35e6b30e8aca58acd7a043afb3
pd-escher <dhoolia.pranav@gmail.com>
2020-08-16 15:03:31 +0530

    ui changes

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -3,14 +3,16 @@ import cx from 'classnames'
 
 const contact = () => (
     <div className={styles.container}>
-        <h1>Contact Us</h1>
-        <form className={cx(styles.grid, styles.card)}>
-            <div className={styles.label}>Name:</div><div className={styles.input}><input className={styles.field}/></div>
-            <div className={styles.label}>Email:</div><div className={styles.input}><input className={styles.field}/></div>
-            <div className={styles.label}>Phone:</div><div className={styles.input}><input className={styles.field}/></div>
-            <div className={styles.label}>Description:</div><div className={styles.input}><textarea className={styles.textarea}/></div>
+        <div className={styles.card}>
+            <h1>Contact Us</h1>
+            <form className={cx(styles.grid, styles.form)}>
+                <div className={styles.label}>Name:</div><div className={styles.input}><input type="name" className={styles.field} /></div>
+                <div className={styles.label}>Email:</div><div className={styles.input}><input type="email" className={styles.field} /></div>
+                <div className={styles.label}>Phone:</div><div className={styles.input}><input type="phone" className={styles.field} /></div>
+                <div className={styles.label}>Description:</div><div className={styles.input}><textarea className={styles.textarea} /></div>
+            </form>
             <div className={styles.submit}><button class="btn2">Submit</button></div>
-        </form>
+        </div>
     </div>
 )
 
