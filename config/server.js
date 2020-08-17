c73fe72fa77895fce539100b5236e76b2ab66698
pd-escher <dhoolia.pranav@gmail.com>
2020-08-17 19:29:55 +0530

    remove config

--- config/server.js
+++
@@ -1,3 +0,0 @@
-export default () => (process.env.NODE_ENV == "production") 
-    ? "https://manipalcartonsbackend.herokuapp.com"
-    : "http://localhost:1337"
--- pages/api/send.js
+++ pages/api/send.js
@@ -1,6 +1,14 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import Axios from 'axios';
 
-import send from "../../config/axios"
+const server = () => (process.env.NODE_ENV == "production") 
+    ? "https://manipalcartonsbackend.herokuapp.com"
+    : "http://localhost:1337"
+
+const axios = Axios.create({
+    baseURL: server(),
+});
+
+const send = async (endpoint, data) => await axios.post(endpoint, data).catch()
 
 export default async (req, res) => {
   await send(req.body.endpoint, req.body.data)
