7b02c0f349ced549f875c8262f17a2bf92097459
pd-escher <dhoolia.pranav@gmail.com>
2020-08-17 19:46:08 +0530

    error logs

--- pages/api/send.js
+++ pages/api/send.js
@@ -8,11 +8,11 @@ const axios = Axios.create({
     baseURL: server(),
 });
 
-const send = async (endpoint, data) => await axios.post(endpoint, data).catch(e => e)
+const send = async (endpoint, data) => await axios.post(endpoint, data).catch(e => { console.error(`ERROR: ${e}`) })
 
 export default async (req, res) => {
   await send(req.body.endpoint, req.body.data)
-    .catch(e => { console.log(e); })
+    .catch(e => { console.error(`ERROR: ${e}`) })
 
   res.statusCode = 200
   res.send("done")
