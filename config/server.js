e4599681b2ef26daa2cd1b60257803e0c2d2f0ba
pd-escher <dhoolia.pranav@gmail.com>
2020-08-17 16:11:24 +0530

    minor changes

--- config/server.js
+++ config/server.js
@@ -1,3 +1,3 @@
 export default () => (process.env.NODE_ENV == "production") 
-    ? "https://manipalcartonsbackend.herokuapp.com"
+    ? "http://manipalcartonsbackend.herokuapp.com"
     : "http://localhost:1337"
