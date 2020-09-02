5b72ec739097259aa7ff3867da4c227bfa169457
pd-escher <dhoolia.pranav@gmail.com>
2020-09-02 13:56:31 +0530

    add backup database support

--- pages/api/send.js
+++ pages/api/send.js
@@ -1,14 +1,17 @@
-import Axios from 'axios';
+import axios from 'axios';
 
-const server = () => (process.env.NODE_ENV == "production") 
-    ? "https://manipalcartonsbackend.herokuapp.com"
-    : "http://localhost:1337"
+const server = () => (process.env.NODE_ENV != "production") 
+    ? ["https://manipalcartonsbackend.herokuapp.com", "https://manipalcartonsbackup.herokuapp.com"]
+    : ["http://localhost:1337"]
 
-const axios = Axios.create({
-    baseURL: server(),
-});
+const requests = (endpoint) => (data) => server().map(url => () => axios.post(`${url}${endpoint}`, data))
 
-const send = async (endpoint, data) => await axios.post(endpoint, data).catch(e => { console.error(`ERROR: ${e}`) })
+const send = async (endpoint, data) => {
+  console.log(requests(endpoint)(data))
+  await axios.all(
+    requests(endpoint)(data).map(fn => fn())
+  )
+}
 
 export default async (req, res) => {
   await send(req.body.endpoint, req.body.data)
